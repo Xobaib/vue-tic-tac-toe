9f4d40be335ff90344d1be6001c6ed7171d4503c
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-04-28 22:29:54 +0430

    template of the app

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,34 @@ const resetGame = () => {
 };
 </script>
 
-<template></template>
+<template>
+  <main class="pt-8 text-center bg-gray-800 min-h-screen text-white">
+    <h1 class="mb-8 text-3xl font-bold uppercase">Tic Tac Toe</h1>
+    <h3 class="text-xl mb-4">Player {{ player }}'s turn</h3>
+    <div class="flex flex-col items-center mb-8">
+      <div v-for="(row, x) in board" :key="x" class="flex">
+        <div
+          v-for="(cell, y) in row"
+          :key="y"
+          @click="makeMove(x, y)"
+          :class="`border border-white w-20 h-20 hover:bg-gray-700 flex items-center justify-center material-icons-outlined text-4xl cursor-pointer ${
+            cell === 'X' ? 'text-pink-500' : 'text-blue-400'
+          }`"
+        >
+          {{ cell === "X" ? "close" : cell === "O" ? "circle" : "" }}
+        </div>
+      </div>
+    </div>
+    <h2 v-if="winner" class="text-6xl font-bold mb-8">
+      Player '{{ winner }}' wins!
+    </h2>
+    <button
+      @click="resetGame"
+      class="px-4 py-2 bg-pink-500 rounded uppercase font-bold hover:bg-pink-600 duration-300"
+    >
+      reset game
+    </button>
+  </main>
+</template>
 
 <style scoped></style>
